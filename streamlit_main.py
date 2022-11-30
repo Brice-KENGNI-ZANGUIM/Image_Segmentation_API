90d127e661f0bbd98fc70ae6ce3e41a8a1ad70c0
Brice KENGNI ZANGUIM <kenzabri2@yahoo.com>
2022-11-30 07:09:11 +0100

    update de l'indisponibilité des modèles UNET et LINKNET

--- streamlit_main.py
+++ streamlit_main.py
@@ -22,15 +22,18 @@ st.write("- 12 paires (image, masque ) sont disponible par defaut dans le repert
 st.write("- Les noms de ces images et masques sont de la forme : 'Image_[indice].png' et 'Mask_[indice].png' où 'indice' est un entier entre 0 et 11")
 st.write("- Vous pouvez par exemple fournir comme chemins :  'path/Image_2.png'  et 'path/Mask_2.png' respectivement pour l'image et le masque associé")
 
+
 ########################################################################
 ###########     acquisition du nom de modèle à utiliser      ###########
 ########################################################################
+st.write("##### Attention à ne pas utiliser les modèles UNET et LinkNET car les fichiers sauvegardes de ces modèles .h5 n'ont pas pu être chargés sur Github à cause de leurs tailles suppérieures à 100 Mo; ce qui est au delà de la limite admise sur Github. Par conséquent, le seul modèle disponible est le modèle PspNet")
 model_name = st.selectbox(
                             label = "1 -  Quel modèle désirez vous utiliser ?",
                             options = ("Unet", "LinkNet", "PspNet"),
-                            index = 0 ,
+                            index = 2 ,
                             
                          ) 
+
 
 #####################################################################
 ###########     acquisition chemin d'accès à l'image      ###########
